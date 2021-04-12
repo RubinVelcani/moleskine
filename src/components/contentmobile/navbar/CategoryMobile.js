87e7ebe6ba46ebb6dd06662e7fbe11c3aef9f379
RubinVelcani <rubin.velcani@gmail.com>
2021-04-12 13:41:40 +0200

    'only slide out'

--- src/components/contentmobile/navbar/CategoryMobile.js
+++ src/components/contentmobile/navbar/CategoryMobile.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react'
 
 import SectionMobile from './SectionMobile'
-import LastSubmenuItem from '../../contentdesktop/navbar/submenu/LastSubmenuItem'
 import useOutsideClick from '../../helpers/useOutsideClick'
 
 import ArrowLeft from '../../../icons/ArrowLeft'
@@ -22,17 +21,13 @@ const Category = ({ title, submenu }) => {
 
     return (
         <div ref={ref} className='w-full text-right transition cursor-default'>
-            <div className='flex flex-row items-center justify-between pl-8'>
+            <div onClick={toggle} className='flex flex-row items-center justify-between pl-8'>
                 <div className={showSubMenu ? '' : 'invisible'}>
                     <ArrowLeft />
                 </div>
-                <h1 onClick={toggle} className='pr-4 uppercase text-xl py-6 self-center font-medium tracking-wider'>{title}</h1>
+                <h1 className='pr-4 uppercase text-xl py-6 self-center font-medium tracking-wider'>{title}</h1>
             </div>
             <SectionMobile submenu={submenu} toggle={showSubMenu} />
-            {showSubMenu ?
-                <LastSubmenuItem/>
-                : (null)
-            }
         </div>
     )
 }
--- src/components/contentmobile/navbar/SectionMobile.js
+++ src/components/contentmobile/navbar/SectionMobile.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import UnitMobile from './UnitMobile'
+import style from './SectionMobile.module.css'
+import LastSubmenuItem from '../../contentdesktop/navbar/submenu/LastSubmenuItem'
 
 const Section = ({ submenu, toggle }) => {
 
-
     return (
 
-        <div className={toggle ? 'flex flex-col items-end border-t border-b' :'flex flex-col items-end border-t border-b hidden'}>
+        <div className={toggle ? style.postClick : style.preClick}>
             {
                 submenu.map((decade, i) => {
                     return (<UnitMobile key={i} decade={decade}/>)
                 })
             }
+            <LastSubmenuItem/>
         </div>
     )
 }
